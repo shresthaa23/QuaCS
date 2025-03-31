d3a2a618e316af581c3827b05b6c6d4cb9c8442c
David Sanico <dsanico@umich.edu>
2025-03-31 07:00:51 -0400

    multi-gate functionality added to terminal program

--- QuaCS/src/commandLine/cli.ts
+++ QuaCS/src/commandLine/cli.ts
@@ -5,7 +5,8 @@ import { I_gate, X_gate, Y_gate, Z_gate, H_gate } from '../core/gate.js';
 import { Matrix } from 'mathjs';
 
 // npx tsc --project tsconfig.build.json
-// node dist/command_line/cli.js
+// node dist/command_line/cli.js <---buddy i think this is wrong lmao
+// node dist/commandLine/cli.js
 
 const rl = readline.createInterface({
   input: stdin,
@@ -24,16 +25,30 @@ async function main() {
         H: H_gate,
       };
 
-      const init_state = await rl.question('What quatum state do you want to select (Select between 0, 1, H, V, +, or -)? ');
+      const init_state = await rl.question('What quantum state do you want to select (Select between 0, 1, H, V, +, or -)? ');
       console.log(`The initual quantum state is, |${init_state}⟩!`);
       const q = new QuantumState(init_state);  
 
-      const init_gate = await rl.question('What gate do you want to apply (Select between I, X, Y, Z, or H)? ');
-      const selectedGate: Matrix | undefined = gateMap[init_gate];
-      if (!selectedGate) {
-        console.log("wrong gate bozo you ruined it all restart from scratch")
+      let flag:boolean = true;
+      let gates: Matrix[] = [];
+      while (flag) {
+        const init_gate = await rl.question('What gate do you want to apply (Select between I, X, Y, Z, or H, select DONE when finished)? ');
+        const selectedGate: Matrix | undefined = gateMap[init_gate];
+        if (!selectedGate && !(init_gate == 'DONE')) {
+          console.log("wrong gate bozo you ruined it all restart from scratch")
+        }
+
+        if(selectedGate) {
+          gates.push(selectedGate);
+        } else if (init_gate == 'DONE') {
+          flag = false;
+        }
       }
-      q.applyGate(selectedGate)
+
+      gates.reverse()
+      for (var gate of gates) {
+        q.applyGate(gate);
+      }
 
       const alpha = q.getState_complex()[0];
       const beta = q.getState_complex()[1];
